85583c8effbb014b7b7471548f39642446e7acc3
vickoritz <vickor@attensi.com>
2022-02-22 22:42:10 +0100

    feat: starting on password generator function

--- index.js
+++ index.js
@@ -3,4 +3,27 @@ function lightMode() {
     element.classList.toggle("light-mode")
 }
 
-let passwordItems = []
+let items = ["A", "B", "C", "1", "2", "3"] // items to us to generate password
+let password = []
+let passwordEl = document.getElementById("password-one")
+
+function getRandomItem() {
+    let randomIndex = Math.floor( Math.random() * 4)
+    return items[randomIndex]
+}
+ 
+function generatePassword() {
+    let firstItem = getRandomItem()
+    let secondItem = getRandomItem()
+    let thirdItem = getRandomItem()
+    let forthItem = getRandomItem()
+    password = [firstItem, secondItem, thirdItem, forthItem]
+    renderPassword()
+}
+
+function renderPassword() {
+    passwordEl.textContent = "Password " 
+    for (let i = 0; i < password.length; i++) {
+        passwordEl.textContent +=password[i] + ""
+    }
+}
