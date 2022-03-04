613de753793fa731e481869a8a3256613396fa43
vickoritz <vickor@attensi.com>
2022-03-04 21:01:20 +0100

    lookup incl all array lenght, generate sets pw

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let password = []
 let passwordEl = document.getElementById("password-one")
 
 function getRandomItem() {
-    let randomIndex = Math.floor( Math.random() * 4)
+    let randomIndex = Math.floor( Math.random() * 6)
     return items[randomIndex]
 }
  
@@ -17,13 +17,15 @@ function generatePassword() {
     let secondItem = getRandomItem()
     let thirdItem = getRandomItem()
     let forthItem = getRandomItem()
-    password = [firstItem, secondItem, thirdItem, forthItem]
-    renderPassword()
+    let password = [firstItem, secondItem, thirdItem, forthItem]
+    console.log(password)
+    passwordEl.textContent = password
 }
 
-function renderPassword() {
-    passwordEl.textContent = "Password " 
-    for (let i = 0; i < password.length; i++) {
-        passwordEl.textContent +=password[i] + ""
-    }
-}
+
+// function renderPassword() {
+//     passwordEl.textContent = "Password " 
+//     for (let i = 0; i < password.length; i++) { //from array, get first instance (0), last defined as lenght, and concatinate (upp) with one when writing out from array
+//         passwordEl.textContent +=password[i] + ""
+//     }
+// }
