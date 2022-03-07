4d1d6363e5ac3a707621d8bafabd8c185bdde0c1
vickoritz <vickor@attensi.com>
2022-03-07 23:42:17 +0100

    getRandomItem use all array, getNewPassword simpl

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// TODOs
-//1. Update array to hold all possible chars, incl numbers, special signs, a-z
-//2. 1-click copy password to clipboard
-//3. Add ability to set password lenght
+//TODOs
+//Add one-click copy password to clipboard
+//Add ability to set password lenght
 
 function lightMode() {
     var element = document.body;
@@ -10,6 +9,7 @@ function lightMode() {
 
 let items = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "|", "~"]  // items to us to generate password
 let password = []
+let passwordLenght = 8
 let passwordOneEl = document.getElementById("password-one")
 let passwordTwoEl = document.getElementById("password-two")
 let passwordThreeEl = document.getElementById("password-three")
@@ -24,16 +24,13 @@ function generatePasswords() {
 }
 
 function getRandomItem() {
-    let randomIndex = Math.floor( Math.random() * 6) //TODO: Update so it loops through all of the items in the array to generate password
+    let randomIndex = Math.floor( Math.random() * 51)
     return items[randomIndex]
 }
  
 function getNewPassword() {
-    let firstItem = getRandomItem()
-    let secondItem = getRandomItem()
-    let thirdItem = getRandomItem()
-    let forthItem = getRandomItem()
-    return [firstItem+secondItem+thirdItem+forthItem]
+    let password = getRandomItem() + getRandomItem() + getRandomItem() + getRandomItem() + getRandomItem() + getRandomItem() + getRandomItem() + getRandomItem() + getRandomItem() + getRandomItem() + getRandomItem()
+    return [password]
 }
 
 function firstPassword() {
@@ -55,10 +52,3 @@ function forthPassword() {
     console.log(password)
     passwordFourEl.textContent = getNewPassword()
 }
-
-// function renderPassword() {
-//     passwordEl = password
-//     for (let i = 0; i < password.length; i++) //from array, get first instance (0), last defined as lenght, and concatinate (upp) with one when writing out from array
-//     passwordEl.textContent +=password[i]
-//     console.log([i])
-// }
